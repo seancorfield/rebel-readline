0bb3924ea703da487474a0dc247f24769e92ca51
Sean Corfield <sean@corfield.org>
2018-09-16 16:06:43 -0700

    Fix typo in rebel-readline.clojure.utils

--- rebel-readline/src/rebel_readline/clojure/utils.clj
+++ rebel-readline/src/rebel_readline/clojure/utils.clj
@@ -24,9 +24,9 @@
     (.startsWith (str ns) "clojure.")
     (cond-> "https://clojuredocs.org/"
       ns (str ns)
-      name (str "/" (string/replace name #"\?$" "_p"))
-     (.startsWith (str ns) "cljs.")
-     (cond-> "http://cljs.github.io/api/"
+      name (str "/" (string/replace name #"\?$" "_p")))
+    (.startsWith (str ns) "cljs.")
+    (cond-> "http://cljs.github.io/api/"
        ns (str ns)
        name (str "/" (cljs-api-encode-name name)))
-     :else nil)))
+    :else nil))
